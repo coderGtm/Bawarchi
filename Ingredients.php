2fe1212fef86b545d890766a56f5a7a20b9a306a
coderGtm <66418526+coderGtm@users.noreply.github.com>
2022-02-13 19:05:23 +0530

    Create Ingredients.php

---
+++ Ingredients.php
@@ -0,0 +1,119 @@
+<html>
+
+<head>
+
+<title>Select Ingredients</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" type="text/css" href="style/Ingredients.css"/>
+<link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
+
+<style>
+    .poweredBy {
+          
+          position: fixed;
+          margin-left: -100px;
+          bottom: 130px;
+          left: 0px;
+          -ms-transform: rotate(90deg); /* IE 9 */
+          transform: rotate(90deg);
+      }
+</style>
+</head>
+
+<body>
+
+
+<ul>
+<p class="typeLabel">Flours</p>
+       <li><input type="checkbox" id="i0" value="Bajra Flour" name="ingCard"  /><label for="i0"><img   src="media/ings/bajraFlour.jpeg" />
+       <div class="title">Bajra Flour</div>
+       </label></li>
+    <li><input type="checkbox" id="i1" value="Wheat flour" name="ingCard" /><label for="i1"><img src="media/ings/dough.jpg" />
+    <div class="title">Wheat flour</div>
+    </label></li>
+    <li><input type="checkbox" id="i31" value="Chana Flour" name="ingCard" /><label for="i31"><img   src="media/ings/chanaFlour.jpeg" />       <div class="title">Chana Flour</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i32" value="KankiKorma Flour" name="ingCard" /><label for="i32"><img   src="media/ings/kankikormaFlour.jpeg" />       <div class="title">KankiKorma Flour</div> </label></li>
+        <li><input type="checkbox" id="i41" value="Rice flour" name="ingCard" /><label for="i41"><img   src="media/ings/riceFlour.jpeg" />       <div class="title">Rice flour</div> </label></li>
+    <li><input type="checkbox" id="i2" value="Rava" name="ingCard" /><label for="i2"><img   src="media/ings/rava.jpeg" />
+    <div class="title">Rava</div>
+    </label></li>
+    
+<p class="typeLabel">Rice and pulses</p>
+    <li><input type="checkbox" id="i3" value="Rice" name="ingCard" /><label for="i3"><img  src="media/ings/rice.jpeg" /> 
+ <div class="title">Rice</div>
+ </label></li>
+  <li><input type="checkbox" id="i29" value="Tuar dal" name="ingCard" /><label for="i29"><img   src="media/ings/tuarDal.jpeg" />       <div class="title">Tuar dal</div> </label></li>
+        <li><input type="checkbox" id="i40" value="Green dal" name="ingCard" /><label for="i40"><img   src="media/ings/greenDal.jpeg" />       <div class="title">Green dal</div> </label></li>
+        <li><input type="checkbox" id="i44" value="Mug dal" name="ingCard" /><label for="i44"><img   src="media/ings/moongDal.jpeg" />       <div class="title">Mug dal</div> </label></li>
+          <li><input type="checkbox" id="i30" value="Mug" name="ingCard" /><label for="i30"><img   src="media/ings/moong.jpeg" />       <div class="title">Mug</div> </label></li>
+       
+       
+<p class="typeLabel">Vegetables</p>
+    <li><input type="checkbox" id="i6" value="Cauliflower" name="ingCard" /><label for="i6"><img   src="media/ings/cauliflower.jpeg" />       <div class="title">Cauliflower</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i7" value="Tindora" name="ingCard" /><label for="i7"><img   src="media/ings/tindora.jpeg" />       <div class="title">Tindora</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i8" value="Brinjal" name="ingCard" /><label for="i8"><img   src="media/ings/brinjal.jpeg" />       <div class="title">Brinjal</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i9" value="Potato" name="ingCard" /><label for="i9"><img   src="media/ings/potato.jpeg" />       <div class="title">Potato</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i10" value="Dudhi" name="ingCard" /><label for="i10"><img   src="media/ings/bottleguard.jpeg" />       <div class="title">Dudhi</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i11" value="Cabbage" name="ingCard" /><label for="i11"><img   src="media/ings/cabbage.jpeg" />       <div class="title">Cabbage</div> </label></li>
+        <li><input type="checkbox" id="i43" value="Spinach" name="ingCard" /><label for="i43"><img   src="media/ings/spinach.jpeg" />       <div class="title">Spinach</div> </label></li>
+          <li><input type="checkbox" id="i12" value="Onion" name="ingCard" /><label for="i12"><img   src="media/ings/onion.jpeg" />       <div class="title">Onion</div> </label></li>
+        <li><input type="checkbox" id="i45" value="Garlic" name="ingCard" /><label for="i45"><img   src="media/ings/garlic.jpeg" />       <div class="title">Garlic</div>  </label></li>    <li><input type="checkbox" id="i13" value="Tomato" name="ingCard" /><label for="i13"><img   src="media/ings/tomato.jpeg" />       <div class="title">Tomato</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i14" value="Beat" name="ingCard" /><label for="i14"><img   src="media/ings/beatroot.jpeg" />       <div class="title">Beat</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i15" value="Cucumber" name="ingCard" /><label for="i15"><img   src="media/ings/cucumber.jpeg" />       <div class="title">Cucumber</div> </label></li>
+       <li><input type="checkbox" id="i25" value="Capsicum" name="ingCard" /><label for="i25"><img   src="media/ings/capsicum.jpeg" />       <div class="title">Capsicum</div> </label></li>
+        <li><input type="checkbox" id="i35" value="Carrot" name="ingCard" /><label for="i35"><img   src="media/ings/carrot.jpeg" />      <div class="title">Carrot</div> </label></li>
+       
+<p class="typeLabel">Flavouring and add-ons</p>
+       <li><input type="checkbox" id="i36" value="Methi" name="ingCard" /><label for="i36"><img   src="media/ings/methi.jpeg" />      <div class="title">Methi</div> </label></li>
+       <li><input type="checkbox" id="i19" value="Dates" name="ingCard" /><label for="i19"><img   src="media/ings/dates.jpeg" />       <div class="title">Dates</div> </label></li>
+    <li><input type="checkbox" id="i20" value="Fudina" name="ingCard" /><label for="i20"><img   src="media/ings/fudina.jpeg" />       <div class="title">Fudina</div> </label></li>
+        <li><input type="checkbox" id="i39" value="Coriander" name="ingCard" /><label for="i39"><img   src="media/ings/coriander.jpeg" />      <div class="title">Coriander</div>
+       </label></li>
+           <li><input type="checkbox" id="i21" value="Green Chilli" name="ingCard" /><label for="i21"><img   src="media/ings/greenChilli.jpeg" />       <div class="title">Green Chilli</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i22" value="Ginger" name="ingCard" /><label for="i22"><img   src="media/ings/ginger.jpeg" />       <div class="title">Ginger</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i23" value="Chana" name="ingCard" /><label for="i23"><img   src="media/ings/chana.jpeg" />       <div class="title">Chana</div> </label></li>
+  <li><input type="checkbox" id="i26" value="Pizza-Pasta Sauce" name="ingCard" /><label for="i26"><img   src="media/ings/chilliSauce.jpeg" />       <div class="title">Pizza-Pasta Sauce</div> </label></li>
+
+<p class="typeLabel">Ready-made items</p>
+<li><input type="checkbox" id="i27" value="Pasta Packet" name="ingCard" /><label for="i27"><img   src="media/ings/pasta.jpeg" />       <div class="title">Pasta Packet</div> </label></li>
+       </label></li>    <li><input type="checkbox" id="i28" value="Maggi Packet" name="ingCard" /><label for="i28"><img   src="media/ings/noodles.jpeg" />       <div class="title">Maggi Packet</div> </label></li>
+  
+<p class="typeLabel">Other...</p>
+<li><input type="checkbox" id="i33" value="Eno" name="ingCard" /><label for="i33"><img   src="media/ings/eno.jpeg" />       <div class="title">Eno</div> </label></li>
+        <li><input type="checkbox" id="i42" value="Baking Soda" name="ingCard" /><label for="i42"><img   src="media/ings/bakingSoda.jpeg" />       <div class="title">Baking Soda</div> </label></li>
+           <li><input type="checkbox" id="i34" value="Curd" name="ingCard" /><label for="i34"><img src="media/ings/curd.jpeg" />       <div class="title">Curd</div> </label></li>
+      
+       <li><input type="checkbox" id="i38" value="Biryani Masala" name="ingCard" /><label for="i38"><img src="media/ings/biryaniMasala.jpeg" />      <div class="title">Biryani Masala</div>
+       </label></li>
+      
+</ul>
+
+<button id="done">Done👍</button>
+
+<div class="poweredBy"><a href="https://000webhost.com"><img style="width:0px;" src="media/000webhostBanner.png" /></a></div>
+
+<script src="scripts/meals.js"></script>
+<script src="scripts/Ingredients.js"></script>
+
+
+   //increasing visit count by 1
+   <?php
+$filename = "../visitCount.txt";
+$myfile = fopen($filename, "r") or die("Unable to open file!");
+$content = fread($myfile,filesize($filename));
+fclose($myfile);
+
+$newCount = substr($content,1,-1) + 1;
+
+$toWrite = "~".$newCount."~";
+
+file_put_contents($filename,$toWrite);
+?>
+}
+
+</script>
+</body>
+
+
+</html>
